d86e302afad04c7a5e83f59963a2a57f26f94a82
michael.zikes <michael.zikes@intauria.com>
2021-06-08 11:20:59 +0200

    Service & Dependeny Injection

---
+++ src/app/flight-search/dummy-flight.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Flight } from '../entities/flight';
+import { FlightService } from './flight.service';
+
+@Injectable()
+export class DummyFlightService implements FlightService {
+
+  find(from: string, to: string): Observable<Flight[]> {
+    return of([
+      {
+        id: 999,
+        from: 'London',
+        to: 'New York',
+        date: '',
+        delayed: false
+      }
+    ]).pipe(
+      tap(_ => console.log('Dummy Flight Service in use'))
+    );
+  }
+}
---
+++ src/app/flight-search/flight.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Flight } from '../entities/flight';
+import { DefaultFlightService } from './default-flight.service';
+import { DummyFlightService } from './dummy-flight.service';
+
+@Injectable({
+  providedIn: 'root',
+  useClass: DefaultFlightService
+})
+export abstract class FlightService {
+
+  abstract find(from: string, to: string): Observable<Flight[]>;
+
+}
