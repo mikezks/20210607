bb1f527f71606d0f55ca6e360a91dceb14b9e5f2
michael.zikes <michael.zikes@intauria.com>
2021-06-08 15:54:31 +0200

    Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,7 @@
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { FlightSearchComponent } from './flight-booking/container/flight-search/flight-search.component';
-import { CityPipe } from './shared/pipes/city.pipe';
 import {FlightBookingModule} from "./flight-booking/flight-booking.module";
 import {CoreModule} from "./core/core.module";
 
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {SidebarComponent} from "../sidebar/sidebar.component";
-import {NavbarComponent} from "../navbar/navbar.component";
+import {SidebarComponent} from "./sidebar/sidebar.component";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 
 @NgModule({
